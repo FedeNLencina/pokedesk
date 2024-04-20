e2e7ac12f2d279cce11ebe7489a9c54b976905e4
Federico Lencina <federiconahuellencina@gmail.com>
2024-04-20 17:29:21 -0300

    feat: 🎸 add user pokemon list

--- src/components/views/pokemonsUser/PokemonsUser.tsx
+++ src/components/views/pokemonsUser/PokemonsUser.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import mockList from "../../utils/mocks/pokedexDataMock.json";
+import pikachu from "../../../assets/images/pikachu.jpg";
 
 export default function PokemonsUser() {
-  return <div>Mis pokemones</div>;
+  const [pokemonUserList, setPokemonUserList] = useState(mockList.list);
+
+  return (
+    <div className="container">
+      <div className="row pokemonsPokedex">
+        {pokemonUserList.map((pokemon) => {
+          return (
+            <div className="col-lg-4 col-6 d-flex flex-column justify-content-center pokedexPokemon">
+              <div>
+                <img className="img-fluid" src={pikachu}></img>
+              </div>
+              <div className="text-center">{pokemon.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
