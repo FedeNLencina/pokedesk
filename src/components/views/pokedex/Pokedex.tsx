4f576624e499937e9c1fbc371e7c307a2837892d
Federico Lencina <federiconahuellencina@gmail.com>
2024-05-02 23:44:09 -0300

    feat: 🎸 add fetch api

--- src/components/views/pokedex/Pokedex.tsx
+++ src/components/views/pokedex/Pokedex.tsx
@@ -5,39 +5,59 @@ import pikachu from "../../../assets/images/pikachu.jpg";
 
 export default function Pokedex() {
   const urlApi: string = "https://pokeapi.co/api/v2/pokemon?limit=60&offset=0";
-
+  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonPokedexView, setFirstPokemon] = useState(null);
   useEffect(() => {
     const fetchApi = async (urlApi: string) => {
-      const response = await fetch(urlApi);
-      response.json().then((json) => {
-        const { results } = json;
+      try {
+        const response = await fetch(urlApi);
+        const data = await response.json();
+        const { results } = data;
 
-        //const pokemons = results,map(pokemon)
-      });
+        const pokemons = results.map(async (pokemon: any, index: number) => {
+          const pokemonUrl = await fetch(pokemon.url);
+          const pokemonData = await pokemonUrl.json();
+          const isFirstPokemon = index === 0;
+          const poke = {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            img: pokemonData.sprites?.other?.showdown?.front_default,
+          };
+          if (isFirstPokemon) {
+            setFirstPokemon(poke);
+          }
+
+          return poke;
+        });
+
+        setPokemonList(await Promise.all(pokemons));
+      } catch (error) {
+        console.log(error);
+      }
     };
+
     fetchApi(urlApi);
   }, []);
 
-  const [pokemonList, setPokemonList] = useState(mockList.list);
-  const [pokemonPokedexView, setFirstPokemon] = useState(mockList.list[0]);
   return (
     <div className="container">
       <div className="d-flex justify-content-center row pokemonInfoPokedexConteiner">
         <div className="col-lg-6 col-sm-12 d-flex justify-content-center pokedexPokemonPhoto">
-          <img className="img-fluid" src={pikachu}></img>
+          <img className="img-fluid" src={pokemonPokedexView?.img}></img>
         </div>
         <div className="col-lg-6 col-sm-12 d-flex flex-column justify-content-center align-items-center pokedexPokemonInfo">
-          <div>Nombre: {pokemonPokedexView.name}</div>
-          <div>Tipo: {pokemonPokedexView.info.type}</div>
-          <div>Debilidad: {pokemonPokedexView.info.weakness}</div>
+          <div>Nombre: {pokemonPokedexView?.name}</div>
+          <div>Tipo: {}</div>
+          <div>Debilidad: {}</div>
         </div>
       </div>
       <div className="row pokemonsPokedex">
         {pokemonList.map((pokemon) => {
+          console.log("pokemon data: ", pokemon);
           return (
             <div className="col-lg-4 col-6 d-flex flex-column justify-content-center pokedexPokemon">
               <div>
-                <img className="img-fluid" src={pikachu}></img>
+                <img className="img-fluid" src={pokemon.img}></img>
               </div>
               <div className="text-center">{pokemon.name}</div>
             </div>
